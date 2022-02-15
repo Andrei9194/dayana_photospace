6c9fbaa84f8ac73f3426190534f68ec52a0682cc
Andrei Prakapovich <andre1121311@gmail.com>
2022-02-15 22:27:38 +0100

    add firebase function, carousel react bootstrap to home component

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,8 +1,38 @@
-export const Home = ()=>{
+import "./index.css"
+import { useEffect, useState } from "react"
+import Carousel from 'react-bootstrap/Carousel'
+import { collection, onSnapshot } from "firebase/firestore"
+import db from "../../firebase-config"
 
 
+export const Home =() =>{
 
+    const [images, setImages] = useState(null);
+
+    useEffect(()=>{
+        onSnapshot(collection(db, 'homeImage'), (snapshot)=>{
+            setImages(snapshot.docs.map(doc => doc.data()))
+        })
+    }, [images])
+    
     return(
-        <h1>Home</h1>
+    <div className="home-conatiner">
+  
+          <Carousel>
+              {images && images.map(image =>
+                         
+                <Carousel.Item interval={3000}>
+                    <img
+                className="d-block" 
+                src={image.homeUrl}
+                alt="First slide"
+                style={{width: '100%', height: `${600}px`, display: 'block', pointerEvents: 'none'}}
+                />      
+                </Carousel.Item>
+
+                )}
+        </Carousel>
+    </div>
     )
 }
+
--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,18 +1,18 @@
-import {getFirestore} from 'firebase/firestore'
-import {initialzeApp} from 'firebase/app'
+import {getFirestore} from "firebase/firestore"
+import { initializeApp } from "firebase/app";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyBcp37_kWKuGFmNgbcEwZbmGhiNaUcxPLM",
-    authDomain: "dayana-photospace.firebaseapp.com",
-    databaseURL: "https://dayana-photospace-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "dayana-photospace",
-    storageBucket: "dayana-photospace.appspot.com",
-    messagingSenderId: "415705365780",
-    appId: "1:415705365780:web:ba9595f5766c2f82209601"
-  };
+ const firebaseConfig = {
+  apiKey: "AIzaSyBcp37_kWKuGFmNgbcEwZbmGhiNaUcxPLM",
+  authDomain: "dayana-photospace.firebaseapp.com",
+  databaseURL: "https://dayana-photospace-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "dayana-photospace",
+  storageBucket: "dayana-photospace.appspot.com",
+  messagingSenderId: "415705365780",
+  appId: "1:415705365780:web:ba9595f5766c2f82209601"
+};
 
+const app = initializeApp(firebaseConfig);
 
-const app = initialzeApp(firebaseConfig)
 export const DATABASE_URL = 'https://dayana-photospace-default-rtdb.europe-west1.firebasedatabase.app'
 
 export default getFirestore()
