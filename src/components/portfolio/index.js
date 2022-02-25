a7a67b61f4be853269869d1285908bae0529100c
Andrei Prakapovich <andre1121311@gmail.com>
2022-02-25 22:24:43 +0100

    Created folder with file PortfolioRow, then import it to file index.js

--- src/components/portfolio/index.js
+++ src/components/portfolio/index.js
@@ -1,38 +1,30 @@
-import { useEffect, useState } from "react"
-import {DATABASE_URL} from '../../firebase-config'
+import {useState, useEffect} from "react"
+import './index.css'
+import { DATABASE_URL } from '../../firebase-config'
+import { PortfolioRow } from "./portfolioRow"
 
-export const Portfolio = ()=>{
+export const Portfolio =() =>{
 
     const [images, setImages] = useState(null)
 
     useEffect(()=>{
-        fetch(`${DATABASE_URL}/portfolioImages.json`)
-        .then(r => r.json())
-        .then(data => {
-            const formattedData = Object.keys(data).map(key => ({id: key, ...data[key]}));
-            setImages(formattedData);
-        })
-        .catch(()=>{      
-        })         
-    }, [])
-
+      fetch(`${DATABASE_URL}/portfolioImages.json`)
+      .then(r => r.json())
+      .then(data => {
+          const formattedData = Object.keys(data).map(key => ({id: key, ...data[key]}));
+          setImages(formattedData);
+      })
+      .catch(()=>{      
+      })         
+  }, [])
+  
     return(
         <div className="portfolio-container" >
-        <div className="portfolio-content-image">
-                {images && images.map(image=> 
-                   <div className="portfolio-container" >
-                   <div className="portfolio-content-image">
-                           {images && images.map(image=> 
-                              <div key={image.id} data-id={image.id} className="portfolio-element" data-title={image.title}>
-                              <img src={image[Object.keys(image)[2]]}
-                                  alt={image.title} className='portfolio-img'
-                                  />
-                               </div>            
-                           )}
-                   </div>
-               </div>          
-                )}
+            <div className="portfolio-content-image">
+                    {images && images.map(image=> 
+                      <PortfolioRow image={image}/>            
+                    )}
+            </div>
         </div>
-    </div>
     )
 }
