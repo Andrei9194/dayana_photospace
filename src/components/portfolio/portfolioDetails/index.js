c6f85e3101cab6579d16e12133feb4c23d695f5d
Andrei Prakapovich <andre1121311@gmail.com>
2022-03-10 19:49:12 +0100

    Add modal.js and styles for this component

--- src/components/portfolio/portfolioDetails/index.js
+++ src/components/portfolio/portfolioDetails/index.js
@@ -6,13 +6,37 @@ import { useParams } from 'react-router-dom';
 import { collection, onSnapshot } from 'firebase/firestore';
 import db from '../../../firebase-config'
 import { TabTitle } from '../../../TabTitle';
+import {Modal} from './modal'
 
 export const PortfolioDetails =() =>{
 
 
     const [images, setImages] = useState(null)
+
+    const [isOpen, setIsOpen] = useState(false)
+    const [modalImage, setModalImage] = useState(null)
     const params = useParams()
 
+    const handleModalImage = (imageUrl)=>{
+        setModalImage(imageUrl)
+    }
+
+
+    const handleClose = ()=>{
+        setIsOpen(false)
+        handleModalImage(null)
+    }
+ 
+    const handleOpen=()=>{
+        setIsOpen(!isOpen)
+    }
+
+    console.log(modalImage)
+
+    const handleModalChange =(imageUrl)=>{
+        handleModalImage(imageUrl)
+        handleOpen()
+    }
     useEffect(()=>{
         onSnapshot(collection(db, `${params.id}`), (snapshot) =>{
             setImages( snapshot.docs.map(doc =>doc.data()))
@@ -25,7 +49,10 @@ export const PortfolioDetails =() =>{
         <div className='portfolioDetails-container'>
             <ImageList variant="masonry" className='portfolioDetails-imageList'>
                 {images && images.map((item) => (
-                    <ImageListItem key={item.id} className='portfolioDetails-imageListItem'>
+                    <ImageListItem key={item.id}
+                    onClick={()=>handleModalChange(item.url)}
+
+                    className='portfolioDetails-imageListItem'>
                     <img
                         src={item.url}
                         alt='portfolio image'
@@ -35,6 +62,9 @@ export const PortfolioDetails =() =>{
                     </ImageListItem>
                 ))}
                 </ImageList>
+                <Modal 
+                 open={isOpen} onClose={handleClose} bgImage={modalImage}
+                 />
         </div>
                 
     )
