d480f69ce77122b39ba4b6a6a024bc1a60a9c738
Andrei Prakapovich <andre1121311@gmail.com>
2022-03-12 14:21:36 +0100

    At portfolio css add styles for max-width: 991.98px and zoom effect for all devices

--- src/components/portfolio/portfolioDetails/modal.js
+++ src/components/portfolio/portfolioDetails/modal.js
@@ -4,12 +4,13 @@ export const Modal = ({open, onClose, bgImage})=>{
     if(!open) return null
     return(
         <div className="overLay"  onClick={onClose}>
-          <div onClick={onClose}>
+            <div>
                 <img
                     src={bgImage}
                     alt='portfolio image'
                     className='modal-image'
                     aria-hidden
+                    onClick={onClose}
                 />
             </div>
         </div>
