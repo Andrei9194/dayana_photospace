06852fc9b06cb8f1b2d89835bba6aef810a85327
Andrei Prakapovich <andre1121311@gmail.com>
2022-03-16 22:50:21 +0100

    Correct code

--- src/components/portfolio/portfolioRow/index.js
+++ src/components/portfolio/portfolioRow/index.js
@@ -6,7 +6,6 @@ export const PortfolioRow = ({image}) =>{
         <Link to={`/portfolio/${image.title}`}>
             <div key={image.id} data-id={image.id} className="portfolio-element" data-title={image.title}>
                 <img src={image[Object.keys(image)[2]]}  alt={image.title} className='portfolio-img' />
-                {/* <p className='portfolio-img'>hey</p> */}
             </div>
         </Link>                
     )
